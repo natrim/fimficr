30b39a9662d5df9aa3767f0e7fa9a30ced67368f
Natrim <maly@kinet.cz>
2016-05-02 12:13:56 +0200

    maxed window by def

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ require('Common');
 var window = new Window();
 application.exitAfterWindowsClose = true;
 window.visible = true;
-window.title = "FimFiction.net Reader";
+window.title = 'FimFiction.net Reader';
 
 window.titleVisible = false;
 window.animateOnSizeChange = true;
 window.animateOnPositionChange = true;
+
+window.state = 'maximized';
 
 
 var webview = new WebView();
@@ -26,18 +28,18 @@ homeButton.image = 'home';
 reloadButton.image = 'reload';
 
 // Use 'space' for a OS-determined variable length space between items.
-toolbar.appendChild([backButton, reloadButton, forwardButton, homeButton, 'space', urlLocation, 'space']);
+toolbar.appendChild(['space', backButton, reloadButton, forwardButton, homeButton, 'space', urlLocation, 'space']);
 window.toolbar = toolbar;
 
 
 backButton.addEventListener('click', function () { webview.back(); });
 forwardButton.addEventListener('click', function () { webview.forward(); });
 reloadButton.addEventListener('click', function () { webview.reload(); });
-homeButton.addEventListener('click', function () { webview.location = "https://www.fimfiction.net/"; });
+homeButton.addEventListener('click', function () { webview.location = 'https://www.fimfiction.net/'; });
 
 urlLocation.addEventListener('inputend', function () {
   var url = urlLocation.value;
-  if (url.indexOf('fimfiction.net') === -1) url = "https://www.fimfiction.net/" + url;
+  if (url.indexOf('fimfiction.net') === -1) url = 'https://www.fimfiction.net/' + url;
   webview.location = url;
 });
 
@@ -50,4 +52,4 @@ urlLocation.linewrap = false;
 urlLocation.scrollable = true;
 
 webview.left = webview.right = webview.top = webview.bottom = 0;
-webview.location = "https://www.fimfiction.net/";
+webview.location = 'https://www.fimfiction.net/';
